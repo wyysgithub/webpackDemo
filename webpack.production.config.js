35b07dc751835969da1ae8e9edf19e48d9f18c4d
wangyangyang <gitlab123456>
2019-03-08 16:05:49 +0800

    12，产品阶段的构建、优化插件 ；

---
+++ webpack.production.config.js
@@ -0,0 +1,72 @@
+/**
+ * 产品阶段所用配置
+ */
+
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+/**
+ *
+ * 4.* 版本只能下 4.* 版本的extract-text-webpack-plugin  否则报错
+ * npm install --save-dev extract-text-webpack-plugin@next
+ * 如果报错 npm cache clean --force
+ *
+ */
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+// npm install --save-dev uglifyjs-webpack-plugin
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = {
+    entry: __dirname + "/app/main.js",
+    output: {
+        path: __dirname + "/build",
+        filename: "bundle-[hash].js"
+    },
+    module: {
+        rules: [
+            {
+                test: /(\.jsx|\.js)$/,
+                use: {
+                    loader: 'babel-loader'
+                },
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css/,
+                use: [
+                    {
+                        loader: 'style-loader'
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: true
+                        }
+                    },
+                    {
+                        loader: 'postcss-loader'
+                    }
+
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new webpack.BannerPlugin("版权所有 WangYangYang"),
+        new HtmlWebpackPlugin({
+            template: __dirname + "/app/index.tmpl.html"
+        }),
+        // new webpack.HotModuleReplacementPlugin(),
+        new webpack.optimize.OccurrenceOrderPlugin(), //为组件分配ID，通过这个插件webpack可以分析和优先考虑使用最多的模块，并为它们分配最小的ID
+        // new webpack.optimize.UglifyJsPlugin(),   用于压缩JS代码  4.* 版本UglifyJsPlugin，替换成了optimization.minimize
+        new ExtractTextPlugin("style.css") //分离CSS和JS文件
+    ],
+    optimization: {
+        minimizer: [
+            new UglifyJsPlugin({
+                uglifyOptions: {
+                    compress: false
+                }
+            })
+        ]
+    },
+}
